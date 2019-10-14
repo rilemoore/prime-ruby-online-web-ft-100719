84a82008c21ad963f43c70e73b511130dd386abf
Riley Moore <rilemoore@gmail.com>
2019-10-14 21:52:03 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(number)
+  divider_found = false
+  if(number <= 1)
+    return false
+  end
+  i = 2
+  while(i < number)
+  
+    if(number % i) == 0
+      divider_found = true
+    end
+    i += 1
+  end
+  return !divider_found
+end
